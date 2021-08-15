b98580ac76e0df0bf55a732275d97ce4cdc54bed
Prasoon Balara <69413205+balaraprasoon2909@users.noreply.github.com>
2021-08-15 22:36:47 +0530

    Update cardinalitySorting.java

--- cardinalitySorting.java
+++ cardinalitySorting.java
@@ -13,6 +13,15 @@ class Solution{
         int[] result = new int[nums.length];
         Arrays.sort(nums);
         Map<Integer, List<Integer>> map = new HashMap<>();
-        
+        for(int i : nums){
+            if(!map.containsKey(cardinality(i))) map.put(cardinality(i), new ArrayList<Integer>());
+            map.get(cardinality(i)).add(i);
+        }
+        int index = 0;
+        for(int i=0; i<=32; i++){
+            if(!map.containsKey(i)) continue;
+            for(int num : map.get(i)) result[index++] = num;
+        }
+        return result;
     }
 }
